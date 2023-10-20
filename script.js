b787452ec22915b27f446d2475560f9af53c9b4c
Dr-material <148547869+Dr-material@users.noreply.github.com>
2023-10-20 19:26:58 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,103 @@
+const videos = [];
+fetch('https://raw.githubusercontent.com/Dr-XYZ/material/main/index.csv')
+  .then((response) => response.text())
+  .then((csv) => {
+    const rows = csv.trim().split('\n');
+    const headers = rows.shift().split(',');
+    rows.forEach((row) => {
+      const values = row.split(',');
+      const video = {};
+      headers.forEach((header, i) => (video[header] = values[i]));
+      videos.push(video);
+    });
+
+    
+    rendervideos(generatevideoHTML(videos, 1)); 
+    renderPaginationButtons(videos); 
+  });
+
+function generatevideoHTML(targetvideos, page) {
+  const startIndex = (page - 1) * 10;
+  const endIndex = startIndex + 10;
+  const pagevideos = targetvideos.slice(startIndex, endIndex);
+
+  return pagevideos
+    .map(
+      (video) =>
+        `<a href="${video['連結']}" class="container" target="_blank">
+            <div class="text">
+              <h2>${video['標題']}</h2>
+            </div>
+            <span class="box">
+              <img src="http://img.youtube.com/vi/${video['圖片']}/default.jpg">
+            </span>
+          </a>`
+    )
+    .join('');
+}
+
+function rendervideos(targetHTML) {
+  const videoList = document.querySelector('[data-videos]');
+  videoList.innerHTML = targetHTML;
+}
+
+function renderPaginationButtons(targetvideos) {
+  const totalPages = Math.ceil(targetvideos.length / 10);
+  const paginationContainer = document.querySelector('.pagination');
+  paginationContainer.dataset.currentPage = 1; 
+
+  paginationContainer.innerHTML = ''; 
+
+  for (let page = 1; page <= totalPages; page++) {
+    const button = document.createElement('button');
+    button.textContent = page;
+    button.dataset.page = page;
+    button.classList.add('pagination-button');
+    if (page === 1) {
+      button.classList.add('current-page'); 
+    }
+    paginationContainer.appendChild(button);
+  }
+
+  document.querySelectorAll('.pagination-button').forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const page = parseInt(e.target.dataset.page);
+      const searchInput = document.querySelector('[data-videofinder-form] input[name="search"]');
+      const searchValue = searchInput.value;
+      rendervideos(generatevideoHTML(videosFilter(searchValue), page)); 
+      paginationContainer.dataset.currentPage = page; 
+      updateCurrentPageButtonStyle(paginationContainer, page); 
+    });
+  });
+}
+
+function videosFilter(targetvideo, allvideos = videos) {
+  if (targetvideo.trim() === '') {
+    return allvideos;
+  }
+  return allvideos.filter((video) => {
+    const regex = new RegExp(targetvideo, 'i');
+    return Object.values(video).some((value) => regex.test(value));
+  });
+}
+
+document.querySelector('[data-videofinder-form]').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const searchInput = e.target.search;
+  const searchValue = searchInput.value;
+  rendervideos(generatevideoHTML(videosFilter(searchValue), 1)); 
+  renderPaginationButtons(videosFilter(searchValue)); 
+  searchInput.value = searchValue; 
+});
+
+function updateCurrentPageButtonStyle(paginationContainer, currentPage) {
+  const buttons = paginationContainer.querySelectorAll('.pagination-button');
+  buttons.forEach((button) => {
+    button.classList.remove('current-page');
+  });
+
+  const currentPageButton = paginationContainer.querySelector(`button[data-page="${currentPage}"]`);
+  if (currentPageButton) {
+    currentPageButton.classList.add('current-page');
+  }
+}
